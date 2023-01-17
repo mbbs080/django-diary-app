396510272e79d5c20a6c65ca6d42f1b315961f51
emeka david <githubmail080@gmail.com>
2023-01-17 01:03:00 +0100

    first commit

--- diary/settings.py
+++ diary/settings.py
@@ -40,8 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
-    'django.contrib.staticfiles',
+    'app.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
 ]
 
